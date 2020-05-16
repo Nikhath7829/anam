5893631ec60cde14e86635a18c26aa2b17d496a7
Nikhath7829 <nikhath7090@gmail.com>
2020-05-16 20:19:52 +0530

    Done

--- src/app/adspage/adspage.page.ts
+++ src/app/adspage/adspage.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Plugins,CameraResultType,CameraSource } from '@capacitor/core';
 @Component({
   selector: 'app-adspage',
   templateUrl: './adspage.page.html',
@@ -10,6 +10,20 @@ export class AdspagePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  takephoto()
+  {
+const  {Camera}  = Plugins;
+const result =   Camera.getPhoto({
+quality:50, 
+allowEditing:true,
+source :CameraSource.Camera,
+resultType : CameraResultType.DataUrl
+});
+//this.image = this.domsanitizer.bypassSecurityTrustResourceUrl( result && result.base64Data,)
+
+
+
   }
 
 }
